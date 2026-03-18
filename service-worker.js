87f31ae173df7ea855a0aa3a2ba59f20c640cd5d
yonerbimp-source <yonerbimp@gmail.com>
2026-03-17 23:08:11 -0400

    nueva actualización

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='agenda-universal-pwa-v4';
+const CACHE='agenda-universal-pwa-v5';
 const FILES=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
